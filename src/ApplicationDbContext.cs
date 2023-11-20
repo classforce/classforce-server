a7302dcb2f2e5b3ab5197093552d297b64b6d209
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-20 22:36:05 +0100

    Add ApplicationDbContext class

---
+++ src/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Classforce.Server.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Classforce.Server;
+
+/// <summary>
+/// Represents the application database context.
+/// </summary>
+public sealed class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ApplicationDbContext"/> class with the specified <paramref name="options"/>.
+    /// </summary>
+    /// <param name="options">The options to be used by the database context.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="options"/> are null.</exception>"
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        ArgumentNullException.ThrowIfNull(options, nameof(options));
+    }
+}
