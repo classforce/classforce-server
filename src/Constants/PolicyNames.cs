871bc63eccc0b557b2485dd3ef58004ef9485ab0
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-21 00:28:56 +0100

    Add PolicyNames constants class

---
+++ src/Constants/PolicyNames.cs
@@ -0,0 +1,17 @@
+﻿namespace Classforce.Server.Constants;
+
+/// <summary>
+/// Specifies the names of the authorization policies used in the application.
+/// </summary>
+public static class PolicyNames
+{
+    /// <summary>
+    /// A policy that requires the user to be authenticated.
+    /// </summary>
+    public const string Users = nameof(Users);
+
+    /// <summary>
+    /// A policy that requires the user to be an administrator.
+    /// </summary>
+    public const string Admins = nameof(Admins);
+}
