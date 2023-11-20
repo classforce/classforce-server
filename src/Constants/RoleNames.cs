8b93de697dabeeb2f8f4fb54cd68a699e6c10f40
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-20 23:27:37 +0100

    Add RoleNames constants class

---
+++ src/Constants/RoleNames.cs
@@ -0,0 +1,17 @@
+﻿namespace Classforce.Server.Constants;
+
+/// <summary>
+/// Specifies the names of the roles used in the application.
+/// </summary>
+public static class RoleNames
+{
+    /// <summary>
+    /// The default role for all authenticated users.
+    /// </summary>
+    public const string User = nameof(User);
+
+    /// <summary>
+    /// The highest available role with unrestricted access to all actions, resources, and settings.
+    /// </summary>
+    public const string Admin = nameof(Admin);
+}
