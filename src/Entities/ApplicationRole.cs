2f2191a8c609a70d11846d1c60702a1fe4eedd69
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-20 22:21:13 +0100

    Add ApplicationRole class

---
+++ src/Entities/ApplicationRole.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Classforce.Server.Entities;
+
+/// <summary>
+/// Represents a user role in the identity system.
+/// </summary>
+/// <remarks>
+/// This is an internal database entity. Do not expose this type to clients.
+/// </remarks>
+public sealed class ApplicationRole : IdentityRole<Guid>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ApplicationRole"/> class with the specified <paramref name="name"/>.
+    /// </summary>
+    /// <param name="name">The name of the role.</param>
+    public ApplicationRole(string name) : base(name)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name, nameof(name));
+    }
+}
