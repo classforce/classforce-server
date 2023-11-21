a41ad6bea61f10ca2732d8f19a0f3a4e9036d390
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-21 21:28:21 +0100

    Replace explicit constructor with new C# 12 primary constructor

--- src/ApplicationDbContext.cs
+++ src/ApplicationDbContext.cs
@@ -7,18 +7,9 @@ namespace Classforce.Server;
 /// <summary>
 /// Represents the application database context.
 /// </summary>
-public sealed class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+/// <param name="options">The options to be used by the database context.</param>
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>(options)
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ApplicationDbContext"/> class with the specified <paramref name="options"/>.
-    /// </summary>
-    /// <param name="options">The options to be used by the database context.</param>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="options"/> are null.</exception>"
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-    {
-        ArgumentNullException.ThrowIfNull(options, nameof(options));
-    }
-
     /// <summary>
     /// Gets or sets the <see cref="DbSet{TEntity}"/> of security sessions.
     /// </summary>
