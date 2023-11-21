e74d36726d4699c09da99324750bdb06d91a3e13
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-21 02:00:07 +0100

    Add SecretSizes constants class

---
+++ src/Constants/SecretSizes.cs
@@ -0,0 +1,17 @@
+﻿namespace Classforce.Server.Constants;
+
+/// <summary>
+/// Specifies the sizes of the secret codes, tokens, and keys used in the application.
+/// </summary>
+public static class SecretSizes
+{
+    /// <summary>
+    /// The maximum value of the security code integer (exclusive).
+    /// </summary>
+    public const int MaxSecurityCode = 1000000;
+
+    /// <summary>
+    /// The number of random bytes in generated refresh tokens.
+    /// </summary>
+    public const int RefreshTokenBytes = 512;
+}
