0ea78e3d1a05603e4bc7abce4b0443793b821133
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-21 21:07:16 +0100

    Add VerificationManagerExtension class

---
+++ src/Extensions/VerificationManagerExtension.cs
@@ -0,0 +1,23 @@
+﻿using Classforce.Server.Services;
+
+namespace Classforce.Server.Extensions;
+
+/// <summary>
+/// A class containing an extension method to register the <see cref="VerificationManager"/> service.
+/// </summary>
+public static class VerificationManagerExtension
+{
+    /// <summary>
+    /// Registers the <see cref="VerificationManager"/> service in a <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>
+    /// The same service collection so that multiple calls can be chained.
+    /// </returns>
+    ///  <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> are <see langword="null"/>.</exception>
+    public static IServiceCollection AddVerificationManager(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services, nameof(services));
+        return services.AddScoped<VerificationManager>();
+    }
+}
