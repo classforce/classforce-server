b1126605cbb1732eb3180cb6d81338c3849fba8c
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-21 20:23:53 +0100

    Add IEmailService interface

---
+++ src/Services/IEmailService.cs
@@ -0,0 +1,17 @@
+﻿namespace Classforce.Server.Services;
+
+/// <summary>
+/// Service responsible for sending emails, including verification codes.
+/// </summary>
+public interface IEmailService
+{
+    /// <summary>
+    /// Sends the verification <paramref name="code"/> to the specified email address.
+    /// </summary>
+    /// <param name="recipient">The email address to send the code to.</param>
+    /// <param name="code">The verification code.</param>
+    /// <returns>
+    /// The <see cref="Task"/> that represents the asynchronous operation.
+    /// </returns>
+    Task SendVerificationCodeAsync(string recipient, int code);
+}
