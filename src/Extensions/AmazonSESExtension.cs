d9de7143767338f83281deb7f9bb9a95867ac5a5
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-21 20:28:12 +0100

    Add AmazonSESExtension class

---
+++ src/Extensions/AmazonSESExtension.cs
@@ -0,0 +1,23 @@
+﻿using Classforce.Server.Services;
+
+namespace Classforce.Server.Extensions;
+
+/// <summary>
+/// A class containing an extension method to register the Amazon Simple Service.
+/// </summary>
+public static class AmazonSESExtension
+{
+    /// <summary>
+    /// Registers the <see cref="AmazonSES"/> service in a <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>
+    /// The same service collection so that multiple calls can be chained.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> are null.</exception>
+    public static IServiceCollection AddAmazonSES(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services, nameof(services));
+        return services.AddScoped<IEmailService, AmazonSES>();
+    }
+}
