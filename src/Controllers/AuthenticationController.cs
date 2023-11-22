f59c0ed61aa978fac0eafbe1b60016828cbf502f
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-22 23:37:25 +0100

    Add AuthenticationController class

---
+++ src/Controllers/AuthenticationController.cs
@@ -0,0 +1,27 @@
+﻿using Classforce.Server.Entities;
+using Classforce.Server.Models;
+using Classforce.Server.Services.Managers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Classforce.Server.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public sealed class AuthenticationController(
+    UserManager<ApplicationUser> userManager,
+    VerificationManager verificationManager) : ControllerBase
+{
+    [HttpPost("create-verification")]
+    public async Task<IActionResult> SendCodeAsync(VerificationCreationRequest request)
+    {
+        var user = await userManager.FindByEmailAsync(request.Email);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        await verificationManager.CreateVerificationAsync(user.Id);
+        return NoContent();
+    }
+}
