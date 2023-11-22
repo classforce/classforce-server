bca988992d732a8406a1039fde02a7c601097c19
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-23 00:15:15 +0100

    Add ApplicationController class

---
+++ src/Controllers/ApplicationController.cs
@@ -0,0 +1,31 @@
+﻿using Classforce.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Classforce.Server.Controllers;
+
+/// <summary>
+/// The base class for all Classforce API controllers.
+/// </summary>
+public class ApplicationController : ControllerBase
+{
+    /// <summary>
+    /// Creates a request result with the specified <paramref name="apiError"/>.
+    /// </summary>
+    /// <param name="apiError">The error that occurred while processing the request.</param>
+    /// <returns>
+    /// The created request result containing the API error.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="apiError"/> is <see langword="null"/>.</exception>
+    [NonAction]
+    public ObjectResult ApiError(ApiError apiError)
+    {
+        ArgumentNullException.ThrowIfNull(apiError, nameof(apiError));
+
+        var result = new ObjectResult(apiError)
+        {
+            StatusCode = apiError.StatusCode
+        };
+
+        return result;
+    }
+}
