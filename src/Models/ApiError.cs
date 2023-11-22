0b0f0d5103f9dcea764a8576ff076291a5fcbf4f
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-23 00:06:59 +0100

    Add StatusCode property to ApiError record

--- src/Models/ApiError.cs
+++ src/Models/ApiError.cs
@@ -11,15 +11,22 @@ public sealed record ApiError
     public ApiError() { }
 
     /// <summary>
-    /// Initialized a new instance of the <see cref="ApiError"/> class with the specified <paramref name="title"/> and optional <paramref name="message"/>.
+    /// Initialized a new instance of the <see cref="ApiError"/> class with the specified HTTP <paramref name="statusCode"/>, <paramref name="title"/> and optional <paramref name="message"/>.
     /// </summary>
+    /// <param name="statusCode">The HTTP status code associated with the error.</param>
     /// <param name="title">The title of the error.</param>
     /// <param name="message">The human-readable description of the error.</param>
-    public ApiError(string title, string? message = null)
+    public ApiError(int statusCode, string title, string? message = null)
     {
+        StatusCode = statusCode;
         Title = title;
         Message = message;
     }
+
+    /// <summary>
+    /// Gets or sets the HTTP status code associated with the error.
+    /// </summary>
+    public required int StatusCode { get; init; }
 
     /// <summary>
     /// Gets or sets the title of the error.
