00b2fc795c7e638fd36721678ffe4295192afe7a
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-22 23:36:55 +0100

    Add VerificationCreationRequest model record

---
+++ src/Models/VerificationCreationRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Classforce.Server.Models;
+
+public sealed record VerificationCreationRequest
+{
+    [Required]
+    [EmailAddress]
+    public required string Email { get; init; }
+}
