3e42b65de1fe98c1cdc24201451036b91befacdb
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-23 00:25:59 +0100

    Add CommonApiErrors constants class

---
+++ src/Constants/CommonApiErrors.cs
@@ -0,0 +1,8 @@
+﻿using Classforce.Server.Models;
+
+namespace Classforce.Server.Constants;
+
+public static class CommonApiErrors
+{
+    public static ApiError InvalidEmail => new(StatusCodes.Status400BadRequest, "INVALID_EMAIL", "The provided email address is invalid.");
+}
