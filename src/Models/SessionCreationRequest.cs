86facc572d176bdb7ff87dc60a8c704a0dd3d7eb
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-24 01:30:02 +0100

    Add SessionCreationRequest model record

---
+++ src/Models/SessionCreationRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Classforce.Server.Models;
+
+public sealed record SessionCreationRequest
+{
+    [Required]
+    [EmailAddress]
+    public required string Email { get; init; }
+
+    [Required]
+    [Range(0, 999999)]
+    public required int Code { get; init; }
+}
