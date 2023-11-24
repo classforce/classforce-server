e326be7d74347b34bc309a79e1394c084dc5a2c1
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-24 01:41:47 +0100

    Add errors related to session creation

--- src/Constants/CommonApiErrors.cs
+++ src/Constants/CommonApiErrors.cs
@@ -4,5 +4,9 @@ namespace Classforce.Server.Constants;
 
 public static class CommonApiErrors
 {
+    public static ApiError UnknownEmail => new(StatusCodes.Status401Unauthorized, "UNKNOWN_EMAIL", "There is no user associated with this email address.");
+
     public static ApiError InvalidEmail => new(StatusCodes.Status400BadRequest, "INVALID_EMAIL", "The provided email address is invalid.");
+
+    public static ApiError InvalidVerificationCode => new(StatusCodes.Status401Unauthorized, "INVALID_VERIFICATION_CODE", "The provided verification code is invalid.");
 }
