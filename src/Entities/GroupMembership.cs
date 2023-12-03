d5ddaed30ead1cd37f6ec4e8404cc06e209a5388
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-03 13:32:28 +0100

    Add GroupMembership entity class

---
+++ src/Entities/GroupMembership.cs
@@ -0,0 +1,24 @@
+﻿using Classforce.Server.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Classforce.Server.Entities;
+
+[PrimaryKey(nameof(UserId), nameof(GroupId))]
+public sealed class GroupMembership
+{
+    public required Guid UserId { get; init; }
+
+    public ApplicationUser User { get; } = null!;
+
+    public required Guid GroupId { get; init; }
+
+    public Group Group { get; } = null!;
+
+    public OrganizationRole Role { get; set; }
+
+    public DateTimeOffset FirstJoinTime { get; init; } = DateTimeOffset.UtcNow;
+
+    public DateTimeOffset LastJoinTime { get; set; } = DateTimeOffset.UtcNow;
+
+    public DateTimeOffset LastRoleChangeTime { get; set; } = DateTimeOffset.UtcNow;
+}
