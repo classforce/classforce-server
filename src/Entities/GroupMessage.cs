558803b62ff75ab8f70b2b6947c0f10439a93be6
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-03 13:16:41 +0100

    Add GroupMessage entity class

---
+++ src/Entities/GroupMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Classforce.Server.Entities;
+
+public sealed class GroupMessage
+{
+    public Guid Id { get; init; }
+
+    public required string Content { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTimeOffset CreationTime { get; init; } = DateTimeOffset.UtcNow;
+
+    public DateTimeOffset LastEditTime { get; set; } = DateTimeOffset.UtcNow;
+}
