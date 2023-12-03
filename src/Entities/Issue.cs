c88dc659becb424f053e43f23ac3eaf389e88f2d
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-03 12:27:35 +0100

    Add Issue entity class

---
+++ src/Entities/Issue.cs
@@ -0,0 +1,22 @@
+﻿using Classforce.Server.Enums;
+
+namespace Classforce.Server.Entities;
+
+public sealed class Issue
+{
+    public Guid Id { get; init; }
+
+    public string? Name { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public IssueType Type { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTimeOffset? DueTime { get; set; }
+
+    public DateTimeOffset CreationTime { get; init; } = DateTimeOffset.UtcNow;
+
+    public DateTimeOffset LastEditTime { get; set; } = DateTimeOffset.UtcNow;
+}
