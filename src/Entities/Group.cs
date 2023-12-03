72c9b9cdb04c90edf24c36e401914987b9229d09
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-03 11:28:47 +0100

    Add group entity class

---
+++ src/Entities/Group.cs
@@ -0,0 +1,16 @@
+﻿namespace Classforce.Server.Entities;
+
+public sealed class Group
+{
+    public Guid Id { get; init; }
+
+    public required string Name { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public bool IsDeleted { get; set; }
+
+    public DateTimeOffset CreationTime { get; init; } = DateTimeOffset.UtcNow;
+
+    public DateTimeOffset LastEditTime { get; set; } = DateTimeOffset.UtcNow;
+}
