38b3dcfd95d2749e6243a2f38e8359de59877b50
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-03 14:41:03 +0100

    Configure relationships for new entities

--- src/ApplicationDbContext.cs
+++ src/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-using Classforce.Server.Entities;
+﻿using Classforce.Server.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -48,5 +48,23 @@ public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
 
         _ = modelBuilder.Entity<ApplicationUser>().HasMany(u => u.SecuritySessions).WithOne(s => s.User).HasForeignKey(s => s.UserId);
         _ = modelBuilder.Entity<ApplicationUser>().HasMany(u => u.EmailVerifications).WithOne(e => e.User).HasForeignKey(e => e.UserId);
+        _ = modelBuilder.Entity<ApplicationUser>().HasMany(u => u.OrganizationMemberships).WithOne(m => m.User).HasForeignKey(m => m.UserId);
+        _ = modelBuilder.Entity<ApplicationUser>().HasMany(u => u.GroupMemberships).WithOne(m => m.User).HasForeignKey(m => m.UserId);
+        _ = modelBuilder.Entity<ApplicationUser>().HasMany(u => u.SentDirectMessages).WithOne(m => m.Author).HasForeignKey(m => m.Author);
+        _ = modelBuilder.Entity<ApplicationUser>().HasMany(u => u.ReceivedDirectMessages).WithOne(m => m.Recipient).HasForeignKey(m => m.RecipientId);
+        _ = modelBuilder.Entity<ApplicationUser>().HasMany(u => u.SentGroupMessages).WithOne(m => m.Author).HasForeignKey(m => m.Author);
+        _ = modelBuilder.Entity<ApplicationUser>().HasMany(u => u.SentIssueMessages).WithOne(m => m.Author).HasForeignKey(m => m.Author);
+
+        _ = modelBuilder.Entity<Organization>().HasMany(o => o.Memberships).WithOne(m => m.Organization).HasForeignKey(m => m.OrganizationId);
+        _ = modelBuilder.Entity<Organization>().HasMany(o => o.Groups).WithOne(g => g.Organization).HasForeignKey(g => g.OrganizationId);
+
+        _ = modelBuilder.Entity<Group>().HasMany(g => g.Memberships).WithOne(m => m.Group).HasForeignKey(m => m.GroupId);
+        _ = modelBuilder.Entity<Group>().HasMany(g => g.Students).WithOne(s => s.Group).HasForeignKey(s => s.GroupId);
+        _ = modelBuilder.Entity<Group>().HasMany(g => g.Schedules).WithOne(s => s.Group).HasForeignKey(s => s.GroupId);
+        _ = modelBuilder.Entity<Group>().HasMany(g => g.Issues).WithOne(i => i.Group).HasForeignKey(i => i.GroupId);
+        _ = modelBuilder.Entity<Group>().HasMany(g => g.Messages).WithOne(m => m.Group).HasForeignKey(m => m.GroupId);
+
+        _ = modelBuilder.Entity<Schedule>().HasMany(s => s.Unschedules).WithOne(u => u.Schedule).HasForeignKey(u => u.ScheduleId);
+        _ = modelBuilder.Entity<Issue>().HasMany(i => i.Messages).WithOne(m => m.Issue).HasForeignKey(m => m.IssueId);
     }
 }
