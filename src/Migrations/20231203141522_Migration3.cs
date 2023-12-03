25a18bbc323240ee48b58e39adf304905c1f2254
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-03 15:15:30 +0100

    Add Migration3

---
+++ src/Migrations/20231203141522_Migration3.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Classforce.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_AuthorId",
+                table: "Issues",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Issues_AspNetUsers_AuthorId",
+                table: "Issues",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Issues_AspNetUsers_AuthorId",
+                table: "Issues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Issues_AuthorId",
+                table: "Issues");
+        }
+    }
+}
