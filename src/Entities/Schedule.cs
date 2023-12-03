9a8edd61e775af6afc727845d2bba6123e3b7551
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-03 11:55:06 +0100

    Add Schedule entity class

---
+++ src/Entities/Schedule.cs
@@ -0,0 +1,28 @@
+﻿using Classforce.Server.Enums;
+
+namespace Classforce.Server.Entities;
+
+public sealed class Schedule
+{
+    public Guid Id { get; init; }
+
+    public string? Name { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public string? Location { get; set; }
+
+    public string? Supervisor { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public Frequency Frequency { get; set; }
+
+    public required DateTimeOffset StartTime { get; set; }
+
+    public required DateTimeOffset EndTime { get; set; }
+
+    public DateTimeOffset CreationTime { get; init; } = DateTimeOffset.UtcNow;
+
+    public DateTimeOffset LastEditTime { get; set; } = DateTimeOffset.UtcNow;
+}
