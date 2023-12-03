ccca04fdbd7d1dd9d352cd8e49b859bf2d31890b
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-03 12:04:40 +0100

    Add Unschedule entity class

---
+++ src/Entities/Unschedule.cs
@@ -0,0 +1,16 @@
+﻿namespace Classforce.Server.Entities;
+
+public sealed class Unschedule
+{
+    public Guid Id { get; init; }
+
+    public bool IsDeleted { get; set; }
+
+    public required DateTimeOffset StartTime { get; set; }
+
+    public required DateTimeOffset EndTime { get; set; }
+
+    public DateTimeOffset CreationTime { get; init; } = DateTimeOffset.UtcNow;
+
+    public DateTimeOffset LastEditTime { get; set; } = DateTimeOffset.UtcNow;
+}
