7cf8f4a368b1a1895640b1a0410cbfc5358e0496
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-03 13:17:25 +0100

    Add IssueMessage entity class

---
+++ src/Entities/IssueMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Classforce.Server.Entities;
+
+public sealed class IssueMessage
+{
+    public Guid Id { get; init; }
+
+    public required string Content { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTimeOffset CreationTime { get; init; } = DateTimeOffset.UtcNow;
+
+    public DateTimeOffset LastEditTime { get; set; } = DateTimeOffset.UtcNow;
+}
