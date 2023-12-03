70b2f5cf52f34dda75435e69b26ccd7a476aea81
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-03 11:36:16 +0100

    Add Student entity class

---
+++ src/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace Classforce.Server.Entities;
+
+public sealed class Student
+{
+    public Guid Id { get; init; }
+
+    public required string FistNames { get; set; }
+
+    public string? LastName { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTimeOffset CreationTime { get; init; } = DateTimeOffset.UtcNow;
+
+    public DateTimeOffset LastEditTime { get; set; } = DateTimeOffset.UtcNow;
+}
